39c852e0203dded3229502cf2ff5e180932e42e2
Eric Ahn <wchill1337@gmail.com>
2015-06-16 14:59:45 -0500

    wormhole test

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -52,7 +52,7 @@
 
 	var control = {
 		speedThreshold: 5000,
-		rainingRounds: 250,
+		rainingRounds: 500,
 		timePerUpdate: 60000,
 		useSlowMode: false,
 		minsLeft: 60,
@@ -1245,13 +1245,13 @@
 
 	function useWormholeIfRelevant() {
 		// Check the time before using wormhole.
-		if (!isNearEndGame()) {
+		if (!isNearEndGame() && !(level > control.speedThreshold && level % control.rainingRounds === 0)) {
 			return;
 		}
 		// Check if Wormhole is purchased
 		if (tryUsingItem(ABILITIES.WORMHOLE)) {
 			advLog('Less than ' + control.minsLeft + ' minutes for game to end. Triggering wormholes...', 2);
-		} else if (tryUsingItem(ABILITIES.THROW_MONEY_AT_SCREEN)) {
+		} else if (isNearEndGame() && tryUsingItem(ABILITIES.THROW_MONEY_AT_SCREEN)) {
 			advLog('Less than ' + control.minsLeft + ' minutes for game to end. Throwing money at screen for no particular reason...', 2);
 		}
 	}
