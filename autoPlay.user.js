cceb3dfa9032e78649283baf7e5575b0455e7c99
Eric Ahn <wchill1337@gmail.com>
2015-06-20 11:41:27 -0500

    Version bump

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -2,7 +2,7 @@
 // @name /u/wchill Monster Minigame Auto-script w/ anti-troll
 // @namespace https://github.com/wchill/steamSummerMinigame
 // @description A script that runs the Steam Monster Minigame for you.
-// @version 7.0.0
+// @version 7.0.1
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
@@ -16,7 +16,7 @@
 	"use strict";
 
 	//Version displayed to client, update along with the @version above
-	var SCRIPT_VERSION = '7.0.0';
+	var SCRIPT_VERSION = '7.0.1';
 
 	// OPTIONS
 	var clickRate = 20;
