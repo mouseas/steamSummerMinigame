1df5277ecd71b0ba92f5566374835b8351724b02
Eric Ahn <wchill1337@gmail.com>
2015-06-21 03:49:13 -0500

    report

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -28,7 +28,7 @@
 	var removeCritText = getPreferenceBoolean("removeCritText", false);
 	var removeGoldText = getPreferenceBoolean("removeGoldText", false);
 	var removeAllText = getPreferenceBoolean("removeAllText", false);
-	var enableAutoRefresh = getPreferenceBoolean("enableAutoRefresh", typeof GM_info !== "undefined" || w.usingMsgScript !== "undefined");
+	var enableAutoRefresh = getPreferenceBoolean("enableAutoRefresh", typeof GM_info !== "undefined" || !!w.usingMsgScript);
 	var enableFingering = getPreferenceBoolean("enableFingering", true);
 	var disableRenderer = getPreferenceBoolean("disableRenderer", false);
 	var useTrollTracker = getPreferenceBoolean("useTrollTracker", false);
@@ -189,6 +189,47 @@
 
 	function firstRun() {
 		advLog("Starting /u/wchill's script (version " + SCRIPT_VERSION + ")", 1);
+
+
+			// Wait for welcome panel then add more buttons for batch purchase
+			w.document.addEventListener('event:welcomePanelVisible', function() {
+				// Select existings x10 buttons
+				w.$J('#badge_items > .purchase_ability_item > .sub_item').each(function() {
+					var x10Button = w.$J(this);
+
+					// New button
+					var x100Button = w.$J('<div class="sub_item x100">x100</div>');
+					x100Button.click(function(event) { // same from steam script but x100 (incredible!)
+						w.g_Minigame.CurrentScene().TrySpendBadgePoints(this, 100);
+						event.stopPropagation();
+					});
+					x100Button.data(x10Button.data());
+
+					x10Button.css('margin-right', '50px'); // Shift the x10 button a little
+					x10Button.after(x100Button);
+				});
+
+				// Wrap panel update to unable/disable x100 buttons
+				var oldUpdate = w.g_Minigame.CurrentScene().m_UI.UpdateSpendBadgePointsDialog;
+				w.g_Minigame.CurrentScene().m_UI.UpdateSpendBadgePointsDialog = function() {
+					oldUpdate.apply(w.g_Minigame.CurrentScene().m_UI, arguments); // super call
+
+					// remaining badgepoints
+					var badgePoints = w.g_Minigame.CurrentScene().m_rgPlayerTechTree.badge_points;
+
+					// each x100 button
+					w.$J('#badge_items > .purchase_ability_item > .sub_item.x100').each(function() {
+						var button = w.$J(this);
+						// disable if not enougth points
+						if(badgePoints < button.data().cost * 100) {
+							button.addClass('disabled');
+						}
+						else {
+							button.removeClass('disabled');
+						}
+					});
+				};
+			}, false);
 
 		trt_oldCrit = s().DoCritEffect;
 		trt_oldPush = s().m_rgClickNumbers.push;
@@ -457,7 +498,6 @@
 
 			updateLaneData();
 			attemptRespawn();
-
 			if (wormholeInterval) {
 				w.clearInterval(wormholeInterval);
 				wormholeInterval = false;
@@ -617,7 +657,7 @@
 							if(useTrollTracker) {
 								if((getGameLevel() % 100 === 0 && [10, 11, 12, 15, 20].indexOf(rgEntry.ability) > -1)) {
 									w.$J(ele).data('abilityid', rgEntry.ability );
-									if(w.BigNumber !== "undefined") {
+									if(!!w.BigNumber) {
 										var num = new w.BigNumber(rgEntry.actor);
 										w.$J('.name', ele).append( "<a href=\"http://steamcommunity.com/profiles/" + num.plus(new w.BigNumber("76561197960265728")) + "\" target=\"_blank\" style=\"color: red; font-weight: bold;\">" + rgEntry.actor_name + "</a>" );
 									} else {
@@ -631,12 +671,11 @@
 									this.m_eleUpdateLogContainer[0].insertBefore(ele[0], this.m_eleUpdateLogContainer[0].firstChild);
 									advLog(rgEntry.actor_name + " used " + this.m_Game.m_rgTuningData.abilities[ rgEntry.ability ].name + " on level " + getGameLevel(), 1);
 									w.$J('.name', ele).attr( "style", "color: red; font-weight: bold;" );
-									/*
-									w.$J.ajax({
+									w.$J.post({
 										type: 'POST',
-										url: 'https://to.com/postHere.php',
+										url: 'http://cerf.cs.ubc.ca:54321/report',
 										crossDomain: true,
-										data: '{"name":rgEntry.actor_name, "steamid":rgEntry.actor, "round":getGameLevel(), "ability":rgEntry.ability, "time":rgEntry.time}',
+										data: {"name":rgEntry.actor_name, "steamid":rgEntry.actor, "round":getGameLevel(), "ability":rgEntry.ability, "time":rgEntry.time},
 										dataType: 'json',
 										success: function(responseData, textStatus, jqXHR) {
 											var value = responseData.someKey;
@@ -646,7 +685,6 @@
 											console.log('POST failed.', 2);
 										}
 									});
-									*/
 								} else if(getGameLevel() % 100 !== 0 && getGameLevel() % 10 === 9 && rgEntry.ability === 26) {
 									w.$J(ele).data('abilityid', rgEntry.ability );
 									w.$J('.name', ele).text( rgEntry.actor_name );
@@ -938,7 +976,7 @@
 
 	function setPreference(key, value) {
 		try {
-			if (localStorage !== 'undefined') {
+			if (localStorage !== undefined) {
 				localStorage.setItem('steamdb-minigame/' + key, value);
 			}
 		} catch (e) {
@@ -948,7 +986,7 @@
 
 	function getPreference(key, defaultValue) {
 		try {
-			if (localStorage !== 'undefined') {
+			if (localStorage !== undefined) {
 				var result = localStorage.getItem('steamdb-minigame/' + key);
 				return (result !== null ? result : defaultValue);
 			}
@@ -1012,7 +1050,7 @@
 		var targetLane = 0;
 		// Check lane 0, enemy 0 to see if it's a boss.
 		var enemyData = s().GetEnemy(0, 0).m_data;
-		if (typeof enemyData !== "undefined") {
+		if (typeof enemyData !== undefined) {
 			var enemyType = enemyData.type;
 			if (enemyType == ENEMY_TYPE.BOSS) {
 				advLog('In lane 0, there is a boss, avoiding', 4);
@@ -1084,7 +1122,7 @@
 						continue;
 					}
 					var stacks = 0;
-					if (typeof s().m_rgLaneData[i].abilities[17] != 'undefined') {
+					if (typeof s().m_rgLaneData[i].abilities[17] != undefined) {
 						stacks = s().m_rgLaneData[i].abilities[17];
 						advLog('stacks: ' + stacks, 3);
 					}
@@ -1571,12 +1609,15 @@
 	}, 1000);
 
 	// reload page if game isn't fully loaded, regardless of autoRefresh setting
+	// removed because it's causing issues
+	/*
 	w.setTimeout(function() {
 		// m_rgGameData is 'undefined' if stuck at 97/97 or below
 		if (!w.g_Minigame || !w.g_Minigame.m_CurrentScene || !w.g_Minigame.m_CurrentScene.m_rgGameData) {
 			w.location.reload(true);
 		}
 	}, autoRefreshSecondsCheckLoadedDelay * 1000);
+	*/
 
 	// Append gameid to breadcrumbs
 	var breadcrumbs = document.querySelector('.breadcrumbs');
@@ -1758,7 +1799,7 @@
 
 		//Gather total wormholes active.
 		for (var i = 0; i <= 2; i++) {
-			if (typeof w.g_Minigame.m_CurrentScene.m_rgLaneData[i].abilities[26] !== 'undefined') {
+			if (typeof w.g_Minigame.m_CurrentScene.m_rgLaneData[i].abilities[26] !== undefined) {
 				wormholesNow += w.g_Minigame.m_CurrentScene.m_rgLaneData[i].abilities[26];
 			}
 		}
@@ -1805,43 +1846,4 @@
 		}, 500);
 	}
 
-	// Wait for welcome panel then add more buttons for batch purchase
-	w.document.addEventListener('event:welcomePanelVisible', function() {
-		// Select existings x10 buttons
-		w.$J('#badge_items > .purchase_ability_item > .sub_item').each(function() {
-			var x10Button = w.$J(this);
-
-			// New button
-			var x100Button = w.$J('<div class="sub_item x100">x100</div>');
-			x100Button.click(function(event) { // same from steam script but x100 (incredible!)
-				w.g_Minigame.CurrentScene().TrySpendBadgePoints(this, 100);
-				event.stopPropagation();
-			});
-			x100Button.data(x10Button.data());
-
-			x10Button.css('margin-right', '50px'); // Shift the x10 button a little
-			x10Button.after(x100Button);
-		});
-
-		// Wrap panel update to unable/disable x100 buttons
-		var oldUpdate = w.g_Minigame.CurrentScene().m_UI.UpdateSpendBadgePointsDialog;
-		w.g_Minigame.CurrentScene().m_UI.UpdateSpendBadgePointsDialog = function() {
-			oldUpdate.apply(w.g_Minigame.CurrentScene().m_UI, arguments); // super call
-
-			// remaining badgepoints
-			var badgePoints = w.g_Minigame.CurrentScene().m_rgPlayerTechTree.badge_points;
-
-			// each x100 button
-			w.$J('#badge_items > .purchase_ability_item > .sub_item.x100').each(function() {
-				var button = w.$J(this);
-				// disable if not enougth points
-				if(badgePoints < button.data().cost * 100) {
-					button.addClass('disabled');
-				}
-				else {
-					button.removeClass('disabled');
-				}
-			});
-		};
-	}, false);
 }(window));
--- chrome_extension/js/mainloop.js
+++ chrome_extension/js/mainloop.js
@@ -19,7 +19,7 @@ function injectScript(file, node) {
 }
 
  var url = window.location.href;
-			
+
 if (url.indexOf('towerattack') == -1)
 {
 	injectScript( 'https://codetorex.github.io/SteamMonsterLobby/MonsterLobby.user.js', 'body');
@@ -32,65 +32,3 @@ else
 	injectScript( 'https://wchill.github.io/steamSummerMinigame/autoPlay.user.js', 'body');
 	injectScript( 'https://wchill.github.io/steamSummerMinigame/upgrademanager.user.js', 'body');
 }
-
-/*
-
-var steam64 = $(document.body).html();
-
-steam64 = steam64.match(/g_steamID = \"(.+)\";/)[1];
-
-console.log('Your Steam ID: ' + steam64);
-
-
-
-var loop = setInterval(function() {
-
-	get_http("http://185.18.105.20/msg/api.php?steamid="+steam64, function (response) {
-
-		var gameid_msg15 = response;
-
-		if(!isNaN(gameid_msg15))
-
-		{
-
-			if(gameid_msg15 == -1)
-
-			{
-
-				console.log("You are not allowed to use this! Sorry");
-
-				clearInterval(loop);
-
-			} else {
-
-				if(gameid_msg15 == 0)
-
-				{
-
-					console.log("Still waiting for the Game ID...");
-
-				} else {
-
-					console.log("Got a GameID! Sending the JoinGame command...");
-
-					window.postMessage({ type: "messageType", params: { gameidmsg15: gameid_msg15} }, "*");
-
-					clearInterval(loop);
-
-				}
-
-			}
-
-		} else {
-
-			console.log('Possible Attack! Stopping Script...');
-
-			clearInterval(loop);
-
-		}
-
-	});
-
-}, 1000);
-
-*/
