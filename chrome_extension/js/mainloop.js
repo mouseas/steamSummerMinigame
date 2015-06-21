ab9da3362e21bc6c9b32c2baad9561aa8690887c
codetorex <codetorex@gmail.com>
2015-06-21 05:13:11 +0300

    Loading script in minigame page.

--- chrome_extension/js/mainloop.js
+++ chrome_extension/js/mainloop.js
@@ -17,13 +17,21 @@ function injectScript(file, node) {
     th.appendChild(s);
 
 }
-injectScript( 'https://wchill.github.io/steamSummerMinigame/usingScript.js', 'body');
-injectScript( chrome.extension.getURL('/js/bignumber.js'), 'body');
-injectScript( 'https://codetorex.github.io/SteamMonsterLobby/MonsterLobby.user.js', 'body');
-injectScript( 'https://wchill.github.io/steamSummerMinigame/autoPlay.user.js', 'body');
-injectScript( 'https://wchill.github.io/steamSummerMinigame/upgrademanager.user.js', 'body');
 
-
+ var url = window.location.href;
+			
+if (url.indexOf('towerattack') == -1)
+{
+	injectScript( 'https://codetorex.github.io/SteamMonsterLobby/MonsterLobby.user.js', 'body');
+}
+else
+{
+	injectScript( 'https://wchill.github.io/steamSummerMinigame/usingScript.js', 'body');
+	injectScript( chrome.extension.getURL('/js/bignumber.js'), 'body');
+	injectScript( 'https://codetorex.github.io/SteamMonsterLobby/MonsterLobby.user.js', 'body');
+	injectScript( 'https://wchill.github.io/steamSummerMinigame/autoPlay.user.js', 'body');
+	injectScript( 'https://wchill.github.io/steamSummerMinigame/upgrademanager.user.js', 'body');
+}
 
 /*
 
